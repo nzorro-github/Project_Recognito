542133b169c5c5e4823c0bebb91348ab264690d2
nzorro1978 <71729152+nzorro1978@users.noreply.github.com>
2021-03-17 15:47:46 +0000

    Create lambda_function.py
    Function for the the test of the concept for the AWS project.

---
+++ Lambda/lambda_function.py
@@ -0,0 +1,62 @@
+import json
+import urllib.parse
+import boto3
+import logging
+
+print('Loading function')
+
+s3 = boto3.client('s3')
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def detect_labels(photo, bucket):
+
+    client=boto3.client('rekognition')
+
+    response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':photo}},
+        MaxLabels=3)
+
+    print('Detected labels for ' + photo) 
+    print()   
+    for label in response['Labels']:
+        print ("Label: " + label['Name'])
+        print ("Confidence: " + str(label['Confidence']))
+        print ("Instances:")
+        for instance in label['Instances']:
+            print ("  Bounding box")
+            print ("    Top: " + str(instance['BoundingBox']['Top']))
+            print ("    Left: " + str(instance['BoundingBox']['Left']))
+            print ("    Width: " +  str(instance['BoundingBox']['Width']))
+            print ("    Height: " +  str(instance['BoundingBox']['Height']))
+            print ("  Confidence: " + str(instance['Confidence']))
+            print()
+
+        print ("Parents:")
+        for parent in label['Parents']:
+            print ("   " + parent['Name'])
+        print ("----------")
+        print ()
+    return len(response['Labels']) 
+    
+    
+def lambda_handler(event, context):
+    #print("Received event: " + json.dumps(event, indent=2))
+
+    # Get the object from the event and show its content type
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    
+    logger.info(f'Upload notified of {key} on bucket:{bucket}')
+
+    try:
+        #response = s3.get_object(Bucket=bucket, Key=key)
+        #print("CONTENT TYPE: " + response['ContentType'])
+        #return response['ContentType']
+        
+        return detect_labels(key, bucket)
+        
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        raise e
